7d983e0de5948eec7eba7bf5dd53fa37bf378f1e
Karl Nelson <k.c.nelson7692@gmail.com>
2016-03-09 20:00:52 +1100

    Allow multiple csv parsing

--- data/commsec.py
+++ data/commsec.py
@@ -1,7 +1,12 @@
 import utils
 import os
 import csv as csvutils
-import data import
+import itertools
+from data import timeutils
+from data.datamgmt import DailyDataManager
+from typing import List
+import numpy as np
+
 
 DATA_DIR = utils.res(utils.DATA, "commsec")
 OPENING_PRICE = 0
@@ -25,26 +30,31 @@ class CommsecDataManager(DailyDataManager):
         if not commsec_csvs:
             raise Exception("There are no csvs in the {data_dir} that matched the ticker {ticker}".format(data_dir=DATA_DIR, ticker=ticker))
 
-        map(lambda commsec_csv: read_commsec_csv(self, commsec_csv), commsec_csvs)
+        list(map(self.read_commsec_csv, commsec_csvs))
+
+    def read_commsec_csv(self, commsec_csv):
+        with open(commsec_csv, 'r') as csv:
+            for i, l in enumerate(csvutils.reader(csv.readlines())):
+                if i is 0:
+                    self.point_definition = (l[1:])
+                else:
+                    self.parse_point(l[1:])
+            csv.close()
+            self.reorder(sorted(self.data.keys()))
+
+    def parse_point(self, point: iter):
+        # Format: Date, Opening Price, High Sale Price, Low Sale Price, Closing Price, Total Volume Traded
+        self[timeutils.get_closing_time(point[0])] = (float(point[1]), float(point[2]), float(point[3]), float(point[4]),
+                                                      int(point[5]))
+
+    # TODO Find paper dealing with missing data points in time series
+    def clean(self):
+        for i, (d, point) in enumerate(self):
+            zeroed_prices = [v == 0 for v in list(point)[0:4]]
+
+    def daily_return(self, r: range) -> List[np.array]:
+        values = [np.array(list(itertools.islice(point, r.start, r.stop, r.step))) for (key, point) in self]
+        return [ (values[i] - values[i-1])/values[i-1] for i in range(0, len(values))]
 
 
-def read_commsec_csv(dm: CommsecDataManager, commsec_csv):
-    with open(commsec_csv, 'r') as csv:
-        for i, l in enumerate(csvutils.reader(csv.readlines())):
-            if i is 0:
-                dm.point_definition = (l[1:])
-            else:
-                parse_point(dm, l[1:])
-        csv.close()
-        sorted(dm)
 
-
-def parse_point(dm: CommsecDataManager, point: iter):
-    # Format: Date, Opening Price, High Sale Price, Low Sale Price, Closing Price, Total Volume Traded
-    dm[timeutils.get_closing_time(point[0])] = (float(point[1]), float(point[2]), float(point[3]), float(point[4]),
-                                                 int(point[5]))
-
-wow = CommsecDataManager('WOW')
-wow.read_data()
-for d,v in wow:
-    print(d,v)
