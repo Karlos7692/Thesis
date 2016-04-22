2c80256dfd19556068be0bdccebe20e63ebb202f
Karl Nelson <k.c.nelson7692@gmail.com>
2016-04-22 14:35:57 +1000

    Added Processor Stubs

--- data/datamgmt.py
+++ data/datamgmt.py
@@ -1,135 +1,34 @@
 from abc import abstractmethod, ABCMeta
-from datetime import time
-from collections import OrderedDict
-"""
-Abstract Data Manager classes. To be implemented in each data manager type.
-"""
-
-# Abstract Data Manager. The default class handling any data.
-class OldAbstractDataManager(metaclass=ABCMeta):
-
-    INVALID_POSITION = -1
-
-    @abstractmethod
-    def read_data(self, start_date=None, end_date=None):
-        pass
-
-    def __init__(self, ticker):
-        # date -> data point (tuple)
-        self.ticker = ticker
-        self.data = OrderedDict()
-        self.position = 0
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if not self.has_next():
-            self.reset()
-            raise StopIteration("The data does not have a next.")
-        ret = list(self.data.items())[self.position]
-        self.position += 1
-        return ret
-
-    def __setitem__(self, key, value):
-        # if key in self.data and self.data[key] != value:
-        #     raise Exception("Added key {key} exists, expected {expected} but found {value}"
-        #                     .format(key=key, expected=self.data[key], value=value))
-        #
-        # if key in self.data:
-        #     return
-        self.data[key] = value
-
-    def __len__(self):
-        return len(self.data)
-
-    def __after_last__(self):
-        if self.position > len(self.data):
-            return True
-        return False
-
-    def get_ticker(self):
-        return self.ticker
-
-    def has_next(self,) -> bool:
-        if self.position < len(self.data):
-            return True
-        return False
-
-    def get_position(self) -> int:
-        return self.position
-
-    def reset(self) -> bool:
-        self.position = 0
-
-    def first(self):
-        if len(self) == 0:
-            raise Exception("DataManager must have items to retrieve first item")
-        return list(self.data.items())[0]
-
-    def last(self):
-        if len(self) == 0:
-            raise Exception("DataManager must have items to retrieve first item")
-        return list(self.data.items())[len(self)-1]
-
-    def reorder(self, keys_order):
-        self.data = OrderedDict([(key, self.data[key]) for key in keys_order])
-
-
-class DailyDataManager(OldAbstractDataManager):
-
-    @abstractmethod
-    def read_data(self, start_date=None, end_date=None):
-        pass
-
-    def get_point(self, date):
-        if date in self.data:
-            return self.data[date]
-        return None
-
-    def get_range(self, start_date: time, end_date=None):
-        ran = []
-        if start_date not in self.data:
-            raise Exception("Invalid start date {start_date}".format(start_date=start_date))
-
-        for point in list(self.data.items()):
-            d, v = point
-            if end_date is None and start_date <= d:
-                ran.append(point)
-                continue
-
-            if end_date is not None and start_date <= d <= end_date:
-                ran.append(point)
-                continue
-
-            if d > end_date:
-                break
-
-        return ran
-
-# TODO Create pandas wrapper class to be able to manage data after completion of kalman filter.
-
 import pandas as pd
-
+import json
 
 class AbstractDataManager(metaclass=ABCMeta):
+
+    def __init__(self):
+        self.data = pd.DataFrame([])
 
     @abstractmethod
     def __clean_data__(self):
         pass
 
     @abstractmethod
-    def __transform_data__(self):
+    def __select_features__(self):
         pass
+
+    def features(self):
+        return self.data.columns.values
 
 
 class AbstractDataReaderManager(AbstractDataManager):
 
     def __init__(self, ticker):
+        super().__init__()
         self.ticker = ticker
         self.__read_data__(ticker)
         self.__clean_data__()
         self.__transform_data__()
+        self.__validate_selection__()
+        self.__select_features__()
 
     @abstractmethod
     def __read_data__(self, ticker: str):
@@ -143,17 +42,62 @@ class AbstractDataReaderManager(AbstractDataManager):
     def __transform_data__(self):
         pass
 
-
-class AbstractDataMergeManager(AbstractDataManager):
+    @abstractmethod
+    def __validate_selection__(self):
+        pass
 
     @abstractmethod
-    def __merge__(self):
+    def __select_features__(self):
         pass
+
+
+class AbstractMergeManager(AbstractDataManager):
+
+    def __init__(self):
+        super().__init__()
+        self.__merge__()
+        self.__select_features__()
 
     @abstractmethod
     def __clean_data__(self):
         pass
 
     @abstractmethod
-    def __transform_data__(self):
+    def __merge__(self):
         pass
+
+    @abstractmethod
+    def __select_features__(self):
+        pass
+
+
+class FeatureContract(object):
+
+    def __init__(self, column_names):
+        self.column_names = column_names
+
+
+# TODO
+class FeatureProcessor(object):
+    """
+    Create a Data Manager with all relevant features.
+    Specification: Json of hierarchy of operations needed to transform all relevant data into clean features.
+    {
+        {
+            operation: 'name'
+            args: [args needed for operation]
+            data: [operations] or {operation} (Note read does not require data)
+            provider: Data manager class used to perform this operation
+        }
+    }
+    """
+    pass
+
+
+# TODO
+class ExogenousVariableProcessor(object):
+    """
+    Create a ExoVarManager with all the relevant features.
+    Specification: Json of hierarchy of operations needed to transform exogenous variables.
+    """
+    pass
