f6566af77217e22016f3b553c4421f7d010cb368
Karl Nelson <k.c.nelson7692@gmail.com>
2016-04-24 18:11:44 +1000

    Added RBPF Tests

---
+++ models/mcmc/tests/pf_qual_tests.py
@@ -0,0 +1,73 @@
+from models.mcmc.pf import Particle, ParticleFilter
+from models import probability as prob
+from matplotlib import pyplot as plt
+import random
+import numpy as np
+
+# Four landmarks
+world = np.array([[90, 20],
+                 [25, 30],
+                 [60, 100],
+                 [13, 12]])
+
+world_size = 100
+
+robot_actual_location = np.array([76, 31])
+
+SENSE_NOISE = 4
+
+
+class RobotParticle(Particle):
+
+    def __init__(self, x, y):
+        self.location = np.array([x, y])
+        self.sense_noise = SENSE_NOISE
+
+    def dist(self, point):
+        return np.linalg.norm(self.location - point)
+
+    def measure_likelihood(self, y: np.array, u: np.array) -> float:
+
+        # Measure distance of particle from landmarks.
+        belief_dists = np.array([self.dist(obs) for obs in world])
+        return sum([prob.mvn_likelihood(belief_dists[i], y[i], self.sense_noise) for i in range(len(y))])
+
+    def particle_predict(self, u_t) -> np.array:
+        return self.location
+
+    def __str__(self):
+        return '[x={x}, y={y}]'.format(x=self.location[0], y=self.location[1])
+
+
+def p_to_point(particle):
+    return particle.location[0], particle.location[1]
+
+
+def ps_to_points(particles):
+    return [p_to_point(p)[0] for p in particles], [p_to_point(p)[1] for p in particles]
+
+
+def dist(landmark):
+    return np.linalg.norm(robot_actual_location - landmark)
+
+
+def sense():
+        return np.array([dist(landmark) + np.random.normal(0.0, SENSE_NOISE) for landmark in world])
+
+N = 1000
+init_particles = [RobotParticle(random.uniform(0, world_size), random.uniform(0, world_size)) for i in range(N)]
+(x, y) = ps_to_points(init_particles)
+plt.scatter(x, y)
+z = [point[0] for point in world]
+z2 = [point[1] for point in world]
+plt.scatter(z, z2, c='red')
+plt.scatter([robot_actual_location[0]], [robot_actual_location[1]], c='purple')
+plt.show()
+pf = ParticleFilter(init_particles)
+pf.observe(sense(), None)
+(x, y) = ps_to_points(pf.particles)
+plt.scatter(x, y, c='blue')
+plt.scatter(z, z2, c='red')
+plt.scatter([robot_actual_location[0]], [robot_actual_location[1]], c='purple')
+plt.show()
+print(pf.predict(None))
