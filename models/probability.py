fd72e17cbac4e2e6e7747b4e0f0982d7b00d2d74
Karl Nelson <k.c.nelson7692@gmail.com>
2016-04-25 11:52:52 +1000

    Added workaround to logpdf calulation. Resizes vectors to be 1 dimentional instead of nx1

--- models/probability.py
+++ models/probability.py
@@ -4,7 +4,13 @@ from scipy.linalg import inv
 
 
 def mvn_likelihood(x, mu, Sigma):
-    return multivariate_normal.logpdf(x=x, mean=mu, cov=Sigma)
+    # Work around for multivariate_normal logpdf, since it only accepts dimensions as arrays
+    # Reshape arrays to 1 dim
+    if mu.ndim != 1:
+        (rows, cols) = mu.shape
+        x = x.reshape((rows))
+        mu = mu.reshape((rows))
+    return multivariate_normal.logpdf(x=x, mean=mu, cov=Sigma, allow_singular=True)
 
 def mvn_noise(Sigma):
     mu = np.zeros(shape=(Sigma.shape[0]))
