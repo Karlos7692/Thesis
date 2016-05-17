1acbdae0078533eff53c4b8d4768e6ba33fd31a3
Karl Nelson <k.c.nelson7692@gmail.com>
2016-05-18 08:37:12 +1000

    Deleted useless module timeutils

--- data/timeutils.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timezone, timedelta
-
-
-# TODO for now assume that its commsec asx closing time
-def get_closing_time(date: str):
-    dt = datetime.strptime(date, '%d %b %Y')
-    return dt.replace(hour=14, minute=10, tzinfo=timezone(timedelta(hours=10)))
